34dfcc851337f490a749446ccb4739f527e6f311
Douglas Bates <dmbates@gmail.com>
2013-04-22 15:30:54 -0500

    Switched the demo to a style more suited to the interactive REPL.

--- demo/lme4.jl
+++ demo/lme4.jl
@@ -1,28 +1,25 @@
 ## Some of the examples from the lme4 package for R
 
-using DataFrames, RDatasets, Distributions, MixedModels
+using RDatasets, MixedModels
 
 ds = data("lme4", "Dyestuff")
-fm1 = fit(LMMsimple(:(Yield ~ 1|Batch), ds), true);
-show(fm1)
+fm1 = LMMsimple(:(Yield ~ 1|Batch), ds)
+
+ds2 = data("lme4", "Dyestuff2")
+fm1a = LMMsimple(:(Yield ~ 1|Batch), ds2)
 
 psts = data("lme4", "Pastes")
-fm2 = fit(LMMsimple(:(strength ~ (1|sample) + (1|batch)), psts), true);
-show(fm2)
+fm2 = LMMsimple(:(strength ~ (1|sample) + (1|batch)), psts)
 
 pen = data("lme4", "Penicillin")
-fm3 = fit(LMMsimple(:(diameter ~ (1|plate) + (1|sample)), pen), true);
-show(fm3)
+fm3 = LMMsimple(:(diameter ~ (1|plate) + (1|sample)), pen)
 
 chem = data("mlmRev", "Chem97")
-fm4 = fit(LMMsimple(:(score ~ (1|school) + (1|lea)), chem), true);
-show(fm4)
+fm4 = LMMsimple(:(score ~ (1|school) + (1|lea)), chem)
 
-fm5 = fit(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem), true);
-show(fm5)
+fm5 = LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)
 
-fm6 = fit(reml(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)), true);
-show(fm6)
+fm6 = reml(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem))
 
 @elapsed fm6 = fit(reml(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)))
 
