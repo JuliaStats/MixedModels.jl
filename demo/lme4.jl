f08dc7cdd8960429da96380104ce14246a12face
Douglas Bates <dmbates@gmail.com>
2013-06-19 15:06:56 -0500

    Bring the demo up to date.

--- demo/lme4.jl
+++ demo/lme4.jl
@@ -3,24 +3,33 @@
 using RDatasets, MixedModels
 
 ds = data("lme4", "Dyestuff")
-fm1 = LMMsimple(:(Yield ~ 1|Batch), ds)
+fm1 = fit(lmer(:(Yield ~ 1|Batch), ds))
+println(fm1)
 
 ds2 = data("lme4", "Dyestuff2")
-fm1a = LMMsimple(:(Yield ~ 1|Batch), ds2)
+fm1a = fit(lmer(:(Yield ~ 1|Batch), ds2))
+println(fm1a)
 
 psts = data("lme4", "Pastes")
-fm2 = LMMsimple(:(strength ~ (1|sample) + (1|batch)), psts)
+fm2 = fit(lmer(:(strength ~ (1|sample) + (1|batch)), psts))
+println(fm2)
 
 pen = data("lme4", "Penicillin")
-fm3 = LMMsimple(:(diameter ~ (1|plate) + (1|sample)), pen)
+fm3 = fit(lmer(:(diameter ~ (1|plate) + (1|sample)), pen))
+println(fm3)
 
 chem = data("mlmRev", "Chem97")
-fm4 = LMMsimple(:(score ~ (1|school) + (1|lea)), chem)
+fm4 = fit(lmer(:(score ~ (1|school) + (1|lea)), chem))
+println(fm4)
 
-fm5 = LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)
+fm5 = fit(lmer(:(score ~ gcsecnt + (1|school) + (1|lea)), chem))
+println(fm5)
 
-fm6 = reml(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem))
+@time fm6 = fit(reml!(lmer(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)))
+println(fm6)
 
-@elapsed fm6 = fit(reml(LMMsimple(:(score ~ gcsecnt + (1|school) + (1|lea)), chem)))
+inst = data("lme4", "InstEval")
+@time fm7 = fit(lmer(:(y ~ dept*service + (1|s) + (1|d)), inst))
+println(fm7)
 
 
