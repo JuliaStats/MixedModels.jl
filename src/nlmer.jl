6de9beb2d8fb303be35dcd5cee88934758c41cb5
Douglas Bates <dmbates@gmail.com>
2013-08-30 16:23:04 -0500

    More work on the nonlinear regression code.

--- src/nlmer.jl
+++ src/nlmer.jl
@@ -1,9 +1,8 @@
-# data for subject 1 in the theophylline experiment
-# the time zero observation has been removed
 using Base.LinAlg.BLAS: trsv!
 using Base.LinAlg.LAPACK: gemqrt!,geqrt3!, potri!
+using DataFrames, RDatasets, NumericExtensions
 import Distributions.fit, Base.show
-using DataFrames
+import NumericExtensions: evaluate, result_type
 
 #const conc = [2.84, 6.57, 10.5, 9.66, 8.58, 8.36, 7.47, 6.89, 5.94, 3.28]
 
@@ -12,21 +11,86 @@ abstract NonlinearRegModel              # nonlinear regression model
 abstract NLRegJac <: NonlinearRegModel  # nonlinear regression model with Jacobian
 
 abstract NLRegFD <: NonlinearRegModel   # finite-difference nonlinear regression model
+
+function elCol(x::Array,i::Integer)
+    isa(x,Vector) ? x[i] : (isa(x,Matrix) ? x[i,:] : error("x not Vector or Matrix"))
+end
+
+abstract Ptransform
+
+type VClka2Vkka <: Ptransform end
+function pars!{T<:FloatingPoint}(t::VClka2Vkka,out::Matrix{T},in::Matrix{T})
+    copy!(out, in)
+    n,p = size(out); p == 3 || error("3 columns required in 'in' and 'out'")
+    for i in 1:n
+        @inbounds out[i,2] = in[i,2]/in[i,1]
+    end
+    out
+end
+pars{T<:FloatingPoint}(t::VClka2Vkka,in::Matrix{T})=pars!(t,similar(in),in)
+function parsjac!{T<:FloatingPoint}(::VClka2Vkka,out::Matrix{T},jac::Array{T,3},in::Matrix{T})
+    copy!(out, in); n,p = size(out)
+    p == 3 || error("3 columns required in 'in' and 'out'")
+    (n,p,p) == size(jac) || error("dimension mismatch")
+    for i in 1:n
+        V = in[i,1]
+        Cl = in[i,2]
+        out[i,2] = Cl/V
+        jac[i,2,1] = -Cl/(V * V)
+        jac[i,2,2] = 1. / V
+    end
+    out,jac
+end
+pnmsin(::Type{VClka2Vkka}) = ["V","Cl","ka"]
+pnmsout(::Type{VClka2Vkka}) = ["V","k","ka"]
+    
+
+# Oral, single dose, 1 compartment using k and ka as parameters.
+type OralSd1Vkka{T<:FloatingPoint} <: NLRegFD
+    time::Vector{Float64}
+    dose::Float64
+end
+nobs(m::OralSd1Vkka) = length(m.time)
+pnames(m::OralSd1Vkka) = ["V","k","ka"]
+npar(m::OralSd1Vkka) = 3
+
+# Evaluation functor.  Result should be multiplied by dose/V
+type fOralSd1kka <: TernaryFunctor end
+function evaluate{T<:FloatingPoint}(::fOralSd1kka,t::T,k::T,ka::T)
+    ka*(exp(-k*t)-exp(-ka*t))/(ka-k)
+end
+result_type{T<:FloatingPoint}(::fOralSd1kka,::Type{T},::Type{T},::Type{T}) = T
+
+
+function expctd!{T<:FloatingPoint}(mu::Vector{T},m::OralSd1Vkka,x::Array{T})
+    map1!(Multiply(), map!(fOralSd1Vkka(), mu, m.time, elcol(x,2), elcol(x,3), m.dose ./ elcol(x,1)))
+end
 # PK model for single oral dose, 1 compartment with parameters V, Cl and ka
 type OralSd1VClka <: NLRegFD
     time::Vector{Float64}
     dose::Float64
 end
 OralSd1VClka(time::Vector{Float64}) = OralSd1VClka(time,1.) # default is unit dose
-function expctd(m::OralSd1VClka, x::Vector{Float64})
-    if length(x) != 3 error("length(x) = $(length(x)), should be 3") end
-    V = x[1]; Cl = x[2]; ka = x[3]; k = Cl/V;  mult = (m.dose/V)*(ka/(ka-k))
-    [mult * (exp(-k*t)-exp(-ka*t)) for t in m.time]
+nobs(m::OralSd1VClka) = length(m.time)
+pnames(m::OralSd1VClka) = ["V","Cl","ka"]
+npar(m::OralSd1VClka) = 3
+
+function expctd!{T<:Float64}(mu::Vector{T}, m::OralSd1VClka, x::Vector{T})
+    length(x) == 3 || error("trailing dimension of x must be 3")
+    map1!(Multiply(), map!(fOralSd1kka(), mu, m.time, x[2]/x[1], x[3]), m.dose ./ x[1])
 end
-function expctdjac{T<:Float64}(m::OralSd1VClka, x::Vector{T})
-    if length(x) != 3 error("length(x) = $(length(x)), should be 3") end
+
+function expctd!{T<:Float64}(eta::Vector{T}, m::OralSd1VClka, x::Matrix{T})
+    n,p = size(x); p == 3 || error("trailing dimension of x must be 3")
+#    V = x[1,:]; Cl = x[2,:]; ka = x[3,:]; k = Cl./V;
+    map1!(Multiply(), map!(fOralSd1kka(),m.time,x[2,:]./x[1,:],x[3,:]), m.dose./x[1,:])
+end
+expctd{T<:Float64}(m::NonlinearRegModel,x::Vector{T}) = expctd!(Array(T,(nobs(m),)),m,x)
+expctd(m::NonlinearRegModel, x::Matrix{Float64}) = expctd!(Array(T,(nobs(m),)),m,x)
+
+function expctdjac!{T<:Float64}(mu::Vector{T},jac::Matrix{T},m::OralSd1VClka,x::Vector{T})
+    length(x) == 3 || error("length(x) = $(length(x)), should be 3")
     t = m.time; n = length(t); d = m.dose;
-    expctd = Array(Float64,n); jac = Array(Float64, n, 3)
     V = x[1]
     Cl = x[2]
     ka = x[3]
@@ -49,7 +113,7 @@ function expctdjac{T<:Float64}(m::OralSd1VClka, x::Vector{T})
         jac[i,2] = e1 * (ka * e28/e20) * e12 - e5 * (e8 * (e28 * ti))
         jac[i,3] = e1 * (1/e3 - ka/e20) * e12 + e5 * (e11 * ti)
     end
-    expctd, jac
+    mu, jac
 end
 const step = sqrt(eps())
 const steps = [-step, step]
@@ -72,14 +136,13 @@ type NonlinearLS                    # nonlinear least squares problems
     pars::Vector{Float64}
     incr::Vector{Float64}
     obs::Vector{Float64}
-    eta::Vector{Float64}             # expected response
-    resid::Vector{Float64}
+    eta::Vector{Float64}                # expected response
     qtr::Vector{Float64}
     jacob::Matrix{Float64}              # Jacobian matrix
     qr::QR{Float64}
     m::NonlinearRegModel
-    rss::Float64            # residual sum of squares at last successful iteration
-    tolsqr::Float64         # squared tolerance for orthogonality criterion
+    rss::Float64 # residual sum of squares at last successful iteration
+    tolsqr::Float64    # squared tolerance for orthogonality criterion
     minfac::Float64
     mxiter::Int
     fit::Bool
@@ -96,10 +159,8 @@ nlm = NonlinearLS(m, [2.84, 6.57, 10.5, 9.66, 8.58, 8.36, 7.47, 6.89, 5.94, 3.28
 # evaluate expected response and residual at m.pars + fac * m.incr
 # return residual sum of squares
 function updtres(nl::NonlinearLS, fac::Float64)
-    nl.eta = expctd(nl.m, nl.pars + fac * nl.incr)
-    nl.resid = nl.obs - nl.eta; s = 0.
-    for r in nl.resid s += r*r end
-    s
+    expctd!(nl.m, nl.eta, nl.pars + fac * nl.incr)
+    sqdiffsum(nl.obs,nl.eta)
 end
     
 # Create the QR factorization, qtr = Q'resid, solve for the increment and
