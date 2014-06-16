e6e5e73dfcda9a4a93e1c36bf5ce66869c8c9cdb
Douglas Bates <dmbates@gmail.com>
2014-06-16 10:41:38 -0500

    Export more extractors

--- src/MixedModels.jl
+++ src/MixedModels.jl
@@ -19,13 +19,15 @@ module MixedModels
         grplevels,      # number of levels per grouping factor in mixed-effects models
         isfit,          # predictate to check if a model has been fit
         isnested,       # check if vector f is nested in vector g
+        isscalar,       # are all the random-effects terms in the model scalar?
+        hasgrad,        # can the analytic gradient of the objective function be evaluated
         lmm,            # fit a linear mixed-effects model (LMM)
         lower,          # vector of lower bounds on parameters in mixed-effects models
         objective,      # the objective function in fitting a model
         pwrss,          # penalized, weighted residual sum-of-squares
         ranef,          # extract the conditional modes of the random effects
         reml!,          # set the objective to be the REML criterion
-        reml            # is the objective the REML criterion?
+        isreml          # is the objective the REML criterion?
 
     abstract MixedModel          # model with fixed and random effects
     abstract PLSSolver           # type for solving the penalized least squares problem
