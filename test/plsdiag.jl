32fbb278de09dde0393402ea5322220cdb59cea6
Douglas Bates <dmbates@gmail.com>
2014-10-17 12:51:49 -0500

    Loosen criterion on a comparison.

--- test/plsdiag.jl
+++ test/plsdiag.jl
@@ -8,7 +8,7 @@ lmm₈ = lmm(Y ~ 1 + (1|S) + (1|D) + (1+Service|Dept), inst)
 fit(lmm₈)
 
 @test_approx_eq_eps deviance(lmm₈) 237648.14982614366 1.e-3
-@test_approx_eq_eps coef(lmm₈) [3.2684364013777296] 1.e-7
+@test_approx_eq_eps coef(lmm₈) [3.2684364013777296] 1.e-4
 
 lmm₉ = lmm(Y ~ 1 + Service*Dept + (1|S) + (1|D), inst)
 
