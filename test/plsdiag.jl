4a55d2f349c6525c3b49e49a019478a2f714f41a
Douglas Bates <dmbates@gmail.com>
2015-12-04 14:05:43 -0600

    Fix coveralls call (maybe)

--- test/plsdiag.jl
+++
@@ -1,8 +0,0 @@
-lmm₉ = lmm(Y ~ 1 + Service*Dept + (1|S) + (1|D), inst);
-
-@test typeof(lmm₉) == LinearMixedModel{PLSDiag{Int64}}
-@test size(lmm₉) == (73421,28,4100,2)
-
-fit(lmm₉)
-
-@test_approx_eq_eps deviance(lmm₉) 237585.55341516936 1.e-3
