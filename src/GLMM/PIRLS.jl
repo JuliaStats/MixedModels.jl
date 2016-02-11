3c74af45006838b6c681d995b6377b318fc97b3b
Douglas Bates <dmbates@gmail.com>
2016-02-11 11:36:59 -0600

    Beginnings of a glmm function.

--- src/GLMM/PIRLS.jl
+++ src/GLMM/PIRLS.jl
@@ -1,41 +1,30 @@
 ## Penalized iteratively reweighted least squares algorithm for determining the
 ## conditional modes of the random effects in a GLMM
 
-type GeneralizedLinearMixedModel{T} <: MixedModel
+type GeneralizedLinearMixedModel{T <: AbstractFloat, D <: UnivariateDistribution, L <: Link} <: MixedModel
     LMM::LinearMixedModel{T}
-    d::Distribution
-    l::Link
-    wt::Vector{T}
-    η::Vector{T}
-    dμdη::Vector{T}
+    r::GLM.GlmResp{DenseArray{T,1},D,L}
+    β₀::Vector{T}
 end
 
 function glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::Vector, l::Link)
-    LMM = lmm(f,fr)
-    y = model_response(LMM)
-    n = length(y)
-    T = eltype(y)
-    if length(wt) == 0
+    LMM = lmm(f, fr)
+    y = copy(model_response(LMM))
+    if isempty(wt)
         wt = ones(y)
-    elseif length(wt) != n
-        throw(DimensionMismatch("length(wt) should be 0 or length(y)"))
     end
-    if eltype(wt) ≠ T
-        throw(ArgumentError("eltype(wt) must be eltype(y)"))
-    end
-    μ = similar(y)
-    η = similar(y)
-    for i in eachindex(y)
-        μ[i] = mu = mustart(d,y[i],wt[i])
-        η[i] = link(l,mu)
-    end
-    GeneralizedLinearMixedModel(LMM,d,l,wt,η,T[μη(l,eta) for eta in η])
+    X = LMM.trms[end][:,1:end-1]
+    gl = glm(X, y, d, l; wts=wt, offset=zeros(y))
+    β₀ = coef(gl)
+    r = gl.rr
+    Base.A_mul_B!(r.offset, X, β₀)
+    GeneralizedLinearMixedModel(LMM, r, β₀)
 end
 
 function glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::DataVector, l::Link)
     glmm(f,fr,d,convert(Vector,wt),l)
 end
 
-glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::Vector) = glmm(f,fr,d,wt,canonical(d))
+glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::Vector) = glmm(f, fr, d, wt, GLM.canonicallink(d))
 
 glmm(f::Formula, fr::AbstractDataFrame, d::Distribution) = glmm(f,fr,d,Float64[])
