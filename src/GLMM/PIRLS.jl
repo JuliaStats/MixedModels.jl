6818839a84e2a7dfb04d75502b6be50d589c81c4
Douglas Bates <dmbates@gmail.com>
2016-03-10 15:04:07 -0600

    Generalize reweighting of A, move code to its own file

--- src/GLMM/PIRLS.jl
+++ src/GLMM/PIRLS.jl
@@ -33,7 +33,8 @@ function glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::Vector, l:
         A[i, pp1] = Array(T, (sz[i], 1))
         R[i, pp1] = Array(T, (sz[i], 1))
     end
-    reweight!(LMM, r.wrkwts)
+    LMM.weights = copy(r.wrkwts)
+    reweight!(LMM)
     LMM[:θ] = LMM[:θ]        # forces an update of R
     δ = ranef(LMM, true)
     GeneralizedLinearMixedModel(LMM, r, β₀, map(zeros, δ), δ, fe)
@@ -45,59 +46,9 @@ end
 
 glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, wt::Vector) = glmm(f, fr, d, wt, GLM.canonicallink(d))
 
-glmm(f::Formula, fr::AbstractDataFrame, d::Distribution) = glmm(f,fr,d,Float64[])
+glmm(f::Formula, fr::AbstractDataFrame, d::Distribution) = glmm(f, fr, d, Float64[])
 
 lmm(m::GeneralizedLinearMixedModel) = m.LMM
-
-function reweight!{T <: AbstractFloat}(x::LinearMixedModel{T}, wts)
-    trms = x.trms
-    A = x.A
-    if length(wts) ≠ size(trms[1], 1)
-        throw(DimensionMismatch("$(length(wts)) = length(wts) ≠ size(x.trms[1], 1)"))
-    end
-    for j in 1:size(A,2), i in 1:j
-        wtprod!(A[i,j], trms[i], trms[j], wts)
-    end
-    x
-end
-
-function wtprod!{T <: AbstractFloat}(A::Diagonal{T}, ti::ScalarReMat{T}, tj::ScalarReMat{T}, wt::Vector{T})
-    n, q = size(ti)
-    if ti === tj
-        ad = fill!(A.diag,0)
-        z = ti.z
-        if length(ad) ≠ q || length(wt) ≠ n || length(z) != n
-            throw(DimensionMismatch("size(A) should be $q and length(wt) should be $n"))
-        end
-        tir = ti.f.refs
-        for i in eachindex(tir, wt)
-            ad[tir[i]] += abs2(z[i]) * wt[i]
-        end
-        return A
-    end
-    error("Shouldn't happen?")
-end
-
-function wtprod!{T <: AbstractFloat}(A::Matrix{T}, ti::ScalarReMat{T}, tj::Matrix{T}, wt::Vector{T})
-    r, m = size(ti)
-    q, n = size(tj)
-    if r ≠ q || length(wt) ≠ r || size(A, 1) ≠ m || size(A, 2) ≠ n
-        throw(DimensionMismatch("matrix product dimensions"))
-    end
-    fill!(A, 0)
-    z = ti.z
-    tir = ti.f.refs
-    for j in 1:n
-        for k in eachindex(wt)
-            A[tir[k], j] += wt[k] * z[k] * tj[k, j]
-        end
-    end
-    A
-end
-
-function wtprod!{T <: AbstractFloat}(A::Matrix{T}, ti::Matrix{T}, tj::Matrix{T}, wt::Vector{T})
-    Ac_mul_B!(A, ti, scale(wt, tj))
-end
 
 function usolve!(m::GeneralizedLinearMixedModel)
     m
