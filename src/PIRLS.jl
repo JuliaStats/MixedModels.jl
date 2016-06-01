39b505622932ec7efd9e68bac8d3e68c5112aa2b
Douglas Bates <dmbates@gmail.com>
2016-06-01 10:17:47 -0500

    Bernoulli() now allowed as a GLM distribution.

--- src/PIRLS.jl
+++ src/PIRLS.jl
@@ -53,9 +53,9 @@ Create a `GeneralizedLinearMixedModel` object which is ready to be `fit!`
 but has not yet been fit.
 """
 function glmm(f::Formula, fr::AbstractDataFrame, d::Distribution, l::Link; wt=[], offset=[])
-#    if d == Binomial() && isempty(wt)
-#        d = Bernoulli()
-#    end
+    if d == Binomial() && isempty(wt)
+        d = Bernoulli()
+    end
     wts = isempty(wt) ? ones(nrow(fr)) : Array(wt)
         # the weights argument is forced to be non-empty in the lmm as it will be used later
     LMM = lmm(f, fr; weights = wts)
