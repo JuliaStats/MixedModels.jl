437638496c76858ddfed5a6aca04dfefbd6b8b4e
Douglas Bates <dmbates@gmail.com>
2017-02-10 11:58:30 -0600

    Build docs using julia-0.5.0

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,6 @@ makedocs()
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo = "github.com/dmbates/MixedModels.jl.git",
-    julia = "0.4",
+    julia = "0.5",
     osname = "linux"
 )
