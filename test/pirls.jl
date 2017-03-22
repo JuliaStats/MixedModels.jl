d9fb0cf0d5c0270bdd5aeab319a2561bbbb1d78e
Douglas Bates <dmbates@gmail.com>
2017-03-22 14:15:15 -0500

    Add grouseticks example

--- test/pirls.jl
+++ test/pirls.jl
@@ -6,12 +6,12 @@ using Base.Test, DataFrames, MixedModels
     contraception[:urbdist] = string.(contraception[:urb], contraception[:d])
     gm0 = fit!(glmm(@formula(use01 ~ 1 + a + a2 + urb + l + (1 | urbdist)), contraception,
         Bernoulli()), fast=true)
-    @test LaplaceDeviance(gm0) ≈ 2361.6572 atol = 0.001
+    @test LaplaceDeviance(gm0) ≈ 2361.657188518064 atol = 0.001
     gm1 = fit!(glmm(@formula(use01 ~ 1 + a + a2 + urb + l + (1 | urbdist)), contraception,
         Bernoulli()));
     @test gm1.θ[1] ≈ 0.573054 atol=0.005
     @test lowerbd(gm1) == push!(fill(-Inf, 7), 0.)
-    @test LaplaceDeviance(gm1) ≈ 2361.5457555 rtol=0.00001
+    @test LaplaceDeviance(gm1) ≈ 2361.5457542860527 rtol=0.00001
     @test loglikelihood(gm1) ≈ -1180.7729 rtol=0.00001
     # the next three values are not well defined in the optimization
     @test logdet(gm1) ≈ 75.7275 atol=0.1
@@ -23,11 +23,12 @@ end
 @testset "cbpp" begin
     cbpp[:prop] = cbpp[:i] ./ cbpp[:s]
     gm2 = fit!(glmm(@formula(prop ~ 1 + p + (1 | h)), cbpp, Binomial(), LogitLink(), wt = cbpp[:s]));
-    @test LaplaceDeviance(gm2) ≈ 100.095856 atol=0.0001
-    @test sum(abs2, gm2.u[1]) ≈ 9.722723538340022 atol=0.0001
-    @test logdet(gm2) ≈ 16.902308856912597 atol=0.0001
-    @test sum(gm2.resp.devresid) ≈ 73.47082400402127 atol=0.001
-    @test loglikelihood(gm2) ≈ -92.0262819685725 atol=0.001
+@show LaplaceDeviance(gm2), sum(abs2, gm2.u[1]), logdet(gm2), sum(gm2.resp.devresid), loglikelihood(gm2)
+    @test LaplaceDeviance(gm2) ≈ 100.09585619324639 atol=0.0001
+    @test sum(abs2, gm2.u[1]) ≈ 9.723175126731014 atol=0.0001
+    @test logdet(gm2) ≈ 16.900889129328004 atol=0.0001
+    @test sum(gm2.resp.devresid) ≈ 73.47179193718736 atol=0.001
+    @test loglikelihood(gm2) ≈ -92.02628186555876 atol=0.001
     @test isnan(sdest(gm2))
     @test varest(gm2) == 1
 end
@@ -35,9 +36,19 @@ end
 @testset "verbagg" begin
     verbagg[:r201] = Array(float.(verbagg[:r2] .== "Y"))
     gm3 = fit!(glmm(@formula(r201 ~ 1 + a + g + b + s + (1 | id) + (1 | item)), verbagg, Bernoulli()));
-    @test LaplaceDeviance(gm3) ≈ 8151.399720195352 atol=0.001
+@show LaplaceDeviance(gm3), sum(x->sum(abs2,x),gm3.u), sum(gm3.resp.devresid)
+    @test LaplaceDeviance(gm3) ≈ 8151.39972809092 atol=0.001
     @test lowerbd(gm3) == vcat(fill(-Inf, 6), zeros(2))
     # these two values are not well defined at the optimum
-    @test sum(x -> sum(abs2, x), gm3.u) ≈ 273.2915019321093 atol=0.1
-    @test sum(gm3.resp.devresid) ≈ 7156.546153979343 atol=0.1
+    @test sum(x -> sum(abs2, x), gm3.u) ≈ 273.31563469936697 atol=0.1
+    @test sum(gm3.resp.devresid) ≈ 7156.558983084621 atol=0.1
+end
+
+@testset "grouseticks" begin
+    gm4 = fit!(glmm(@formula(t ~ 1 + y + ch + (1|i) + (1|b) + (1|l)), dat["grouseticks"], Poisson()))
+    @test LaplaceDeviance(gm4) ≈ 849.5439802900257 atol=0.001
+    @test lowerbd(gm4) == vcat(fill(-Inf, 4), zeros(3))
+    # these two values are not well defined at the optimum
+    @test sum(x -> sum(abs2, x), gm4.u) ≈ 196.8695297987013 atol=0.1
+    @test sum(gm4.resp.devresid) ≈ 220.92685781326136 atol=0.1
 end
