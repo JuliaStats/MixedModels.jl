a93d88e8b3eb54d301bc89a57a4b68f6cb6db989
Douglas Bates <dmbates@gmail.com>
2017-08-14 12:51:24 -0500

    A further change for discontinuing v0.5 support

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,6 @@ makedocs()
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo = "github.com/dmbates/MixedModels.jl.git",
-    julia = "0.5",
+    julia = "0.6",
     osname = "linux"
 )
