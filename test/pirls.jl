914c9f47f6948339327e994efd7ab9cb0de11134
Douglas Bates <dmbates@gmail.com>
2017-12-04 13:35:37 -0600

    Correct the test

--- test/pirls.jl
+++ test/pirls.jl
@@ -18,7 +18,7 @@ end
     @test isapprox(gm1.θ[1], 0.573054, atol=0.005)
     @test lowerbd(gm1) == push!(fill(-Inf, 7), 0.)
     @test StatsBase.dof(gm0) == length(gm0.β) + length(gm0.θ)
-    @test StatsBase.nobs(gm0) = 1934
+    @test StatsBase.nobs(gm0) == 1934
     @test isapprox(LaplaceDeviance(gm1), 2361.5457542860527, rtol=0.00001)
     @test isapprox(loglikelihood(gm1), -1180.7729, rtol=0.00001)
     # the next three values are not well defined in the optimization
