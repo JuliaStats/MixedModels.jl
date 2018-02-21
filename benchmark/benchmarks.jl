56dd4af9a35e3f8a264ad37486fba0cc70a37f10
Douglas Bates <dmbates@gmail.com>
2018-02-21 12:57:32 -0600

    Update benchmarks to PkgBenchmark v0.1.0 (#119)
    * Add ml1m data set.  Set up for PkgBenchmark v0.1.0
    
    * remove whitespace in model names
    
    * Fix missed change to compactstr [ci skip]

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,6 @@
-using DataFrames, RData, MixedModels, StatsModels
+using BenchmarkTools, DataFrames, RData, MixedModels, StatsModels
+
+const SUITE = BenchmarkGroup()
 
 const dat = convert(Dict{Symbol,DataFrame}, load(Pkg.dir("MixedModels", "test", "dat.rda")));
 
@@ -16,7 +18,7 @@ const mods = Dict{Symbol,Vector{Expr}}(
     :Demand => [:(1+U+V+W+X+(1|G)+(1|H))],
     :Dyestuff => [:(1+(1|G))],
     :Dyestuff2 => [:(1+(1|G))],
-    :Early => [:(1+U+U&A+(1+U|G))], # variables must be renamed
+    :Early => [:(1+U+U&A+(1+U|G))],
     :Exam => [:(1+A*U+B+(1|G)), :(1+A+B+U+(1|G))],
     :Gasoline => [:(1+U+(1|G))],
     :Gcsemv => [:(1+A+(1|G))],      # variables must be renamed
@@ -36,7 +38,7 @@ const mods = Dict{Symbol,Vector{Expr}}(
     :Penicillin => [:(1+(1|G)+(1|H))],
     :Pixel => [:(1+U+V+(1+U|G)+(1|H))],  # variables must be renamed
     :Poems => [:(1+U+V+W+(1|G)+(1|H)+(1|I))],
-    :Rail => [:(1+(1|G))],         # variables must be renamed
+    :Rail => [:(1+(1|G))],
     :SIMS => [:(1+U+(1+U|G))],
     :ScotsSec => [:(1+A+U+V+(1|G)+(1|H))],
     :Semi2 => [:(1+A+(1|G)+(1|H))],
@@ -57,12 +59,13 @@ const mods = Dict{Symbol,Vector{Expr}}(
     :ergoStool => [:(1+A+(1|G))],
     :gb12 => [:(1+S+T+U+V+W+X+Z+((1+S+U+W)|G)+((1+S+T+V)|H))],
     :grouseticks => [],           # rename variables, glmm needs formula
-    :guimmun => [],               # rename variables, glmm needs formula
+    :guImmun => [],               # rename variables, glmm needs formula
     :guPrenat => [],              # rename variables, glmm needs formula
     :kb07 => [:(1+S+T+U+V+W+X+Z+((1+S+T+U+V+W+X+Z)|G)+((1+S+T+U+V+W+X+Z)|H)),
               :(1+S+T+U+V+W+X+Z+(1|G)+((0+S)|G)+((0+T)|G)+((0+U)|G)+((0+V)|G)+((0+W)|G)+
               ((0+X)|G)+((0+Z)|G)+(1|H)+((0+S)|H)+((0+T)|H)+((0+U)|H)+((0+V)|H)+
               ((0+W)|H)+((0+X)|H)+((0+Z)|H))],
+    :ml1m => [:(1+(1|G)+(1|H))],
     :paulsim => [:(1+S+T+U+(1|H)+(1|G))],  # names of H and G should be reversed
     :sleepstudy => [:(1+U+(1+U|G)), :(1+U+(1|G)+(0+U|G))],
     :s3bbx => [],                 # probably drop this one
@@ -70,46 +73,43 @@ const mods = Dict{Symbol,Vector{Expr}}(
     );
 
 fitbobyqa(rhs::Expr, dsname::Symbol) = fit!(lmm(Formula(:Y, rhs), dat[dsname]))
+compactstr(ds,rhs) = replace(string(ds, ':', rhs), ' ', "")
 
-@benchgroup "simplescalar" ["single", "simple", "scalar"] begin
-    for ds in [:Alfalfa, :AvgDailyGain, :BIB, :Bond, :cake, :Cultivation, :Dyestuff,
-        :Dyestuff2, :ergoStool, :Exam, :Gasoline, :Hsb82, :IncBlk, :Mississippi,
-        :PBIB, :Semiconductor, :TeachingII]
-        for rhs in mods[ds]
-            @bench string(ds, ':', rhs) fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
-        end
+SUITE["simplescalar"] = BenchmarkGroup(["single", "simple", "scalar"])
+for ds in [:Alfalfa, :AvgDailyGain, :BIB, :Bond, :cake, :Cultivation, :Dyestuff,
+    :Dyestuff2, :ergoStool, :Exam, :Gasoline, :Hsb82, :IncBlk, :Mississippi,
+    :PBIB, :Rail, :Semiconductor, :TeachingII]
+    for rhs in mods[ds]
+        SUITE["simplescalar"][compactstr(ds, rhs)] = @benchmarkable fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
     end
 end
 
-@benchgroup "singlevector" ["single", "vector"] begin
-    for ds in [:HR, :Oxboys, :SIMS, :sleepstudy, :Weights, :WWheat]
-        for rhs in mods[ds]
-            @bench string(ds, ':', rhs) fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
-        end
+SUITE["singlevector"] = BenchmarkGroup(["single", "vector"])
+for ds in [:Early, :HR, :Oxboys, :SIMS, :sleepstudy, :Weights, :WWheat]
+    for rhs in mods[ds]
+        SUITE["singlevector"][compactstr(ds, rhs)] = @benchmarkable fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
     end
 end
 
-@benchgroup "nested" ["multiple", "nested", "scalar"] begin
-    for ds in [:Animal, :Chem97, :Genetics, :Pastes, :Semi2]
-        for rhs in mods[ds]
-            @bench string(ds, ':', rhs) fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
-        end
+SUITE["nested"] = BenchmarkGroup(["multiple", "nested", "scalar"])
+for ds in [:Animal, :Chem97, :Genetics, :Pastes, :Semi2]
+    for rhs in mods[ds]
+        SUITE["nested"][compactstr(ds, rhs)] = @benchmarkable fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
     end
 end
 
-@benchgroup "crossed" ["multiple", "crossed", "scalar"] begin
-    for ds in [:Assay, :Demand, :InstEval, :Penicillin, :ScotsSec, :d3,
-               :dialectNL, :egsingle, :paulsim]
-        for rhs in mods[ds]
-            @bench string(ds, ':', rhs) fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
-        end
+
+SUITE["crossed"] = BenchmarkGroup(["multiple", "crossed", "scalar"])
+for ds in [:Assay, :Demand, :InstEval, :Penicillin, :ScotsSec,
+           :dialectNL, :egsingle, :ml1m, :paulsim]
+    for rhs in mods[ds]
+        SUITE["crossed"][compactstr(ds, rhs)] = @benchmarkable fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
     end
 end
 
-@benchgroup "crossedvector" ["multiple", "crossed", "vector"] begin
-    for ds in [:bs10, :gb12, :kb07]
-        for rhs in mods[ds]
-            @bench string(ds, ':', rhs) fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
-        end
+SUITE["crossedvector"] = BenchmarkGroup(["multiple", "crossed", "vector"])
+for ds in [:bs10, :d3, :gb12, :kb07]
+    for rhs in mods[ds]
+        SUITE["crossedvector"][compactstr(ds, rhs)] = @benchmarkable fitbobyqa($(QuoteNode(rhs)), $(QuoteNode(ds)))
     end
 end
