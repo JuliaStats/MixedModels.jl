e09e09fed4af8c84a006b1ec0b36e15c89fcd7f1
Phillip Alday <phillip.alday@mpi.nl>
2019-10-03 23:13:45 +0200

    insane workaround for userlib in R without prompt

--- test/rcall.jl
+++ test/rcall.jl
@@ -5,7 +5,33 @@ const GLMM = GeneralizedLinearMixedModel
 @testset "RCall for lme4" begin
     reval("""
     if(!require(lme4)){
-        install.packages("lme4",repos="https://cloud.r-project.org")
+        # this is insane, but this part of install.packages to use a userlib
+        # because I can't figure out the AppVeyor and Travis configs and otherwise
+        # it hangs at the prompt "do you want to create a user....."
+        lib <- .libPaths()[1L]
+        ok <- dir.exists(lib) & (file.access(lib, 2) == 0L)
+        if (length(lib) == 1L && .Platform\$OS.type == "windows") {
+            ok <- dir.exists(lib)
+            if (ok) {
+                fn <- file.path(lib, paste0("_test_dir_", Sys.getpid()))
+                unlink(fn, recursive = TRUE)
+                res <- try(dir.create(fn, showWarnings = FALSE))
+                if (inherits(res, "try-error") || !res)
+                    ok <- FALSE
+                else unlink(fn, recursive = TRUE)
+            }
+        }
+        if (length(lib) == 1L && !ok) {
+            userdir <- unlist(strsplit(Sys.getenv("R_LIBS_USER"), .Platform\$path.sep))[1L]
+            lib <- userdir
+            if (!file.exists(userdir)) {
+                if (!dir.create(userdir, recursive = TRUE)) stop(gettextf("unable to create %s", sQuote(userdir)), domain = NA)
+                .libPaths(c(userdir, .libPaths()))
+            }
+
+        }
+
+        install.packages("lme4",repos="https://cloud.r-project.org", libs=lib)
         library(lme4)
     }""")
 
