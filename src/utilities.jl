329ac48f3d9baf5bd4d5541fe5c150c4522d4265
Phillip Alday <phillip.alday@mpi.nl>
2020-02-03 23:54:44 +0100

    progress meter

--- src/utilities.jl
+++ src/utilities.jl
@@ -99,15 +99,19 @@ Note that if `f()` is not thread-safe or depends on a non thread-safe RNG,
 """
 function replicate(f::Function, n::Integer; use_threads=false)
     if use_threads
+        # no macro version yet: https://github.com/timholy/ProgressMeter.jl/issues/143
+        p = Progress(n)
         # get the type
         rr = f()
+        next!(p)
         # pre-allocate
         results = [rr for _ in Base.OneTo(n)]
         Threads.@threads for idx = 2:n
             results[idx] = f()
+            next!(p)
         end
     else
-        results = [f() for _ in Base.OneTo(n)]
+        results = @showprogress [f() for _ in Base.OneTo(n)]
     end
     results
 end
