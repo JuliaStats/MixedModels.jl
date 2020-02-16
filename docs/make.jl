3b3f7b54c4b5e1da5d41480e881433b3963c1eda
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2020-02-16 01:41:50 +0100

    Fix with explicit root

--- docs/make.jl
+++ docs/make.jl
@@ -1,37 +1,40 @@
-
 using Documenter, Weave, StatsBase
 using MixedModels
-foreach(filename -> weave(joinpath("docs", "jmd", filename),
-                          doctype = "github",
-                          fig_path = "assets",
-                          fig_ext = ".svg",
-                          out_path = "src"),
-        ["constructors.jmd",
-         "optimization.jmd",
-         "GaussHermite.jmd",
-         "bootstrap.jmd",
-         # "SimpleLMM.jmd",
-         # "MultipleTerms.jmd",
-         # "SingularCovariance.jmd",
-         # "SubjectItem.jmd",
-        ])
+foreach(
+    filename -> weave(
+        joinpath(dirname(pathof(MixedModels)), "..", "docs", "jmd", filename),
+        doctype = "github",
+        fig_path = joinpath("docs", "assets"),
+        fig_ext = ".svg",
+        out_path = joinpath("docs", "src"),
+    ),
+    [
+        "constructors.jmd",
+        "optimization.jmd",
+        "GaussHermite.jmd",
+        "bootstrap.jmd",
+        # "SimpleLMM.jmd",
+        # "MultipleTerms.jmd",
+        # "SingularCovariance.jmd",
+        # "SubjectItem.jmd",
+    ],
+)
 
 makedocs(
+    root = joinpath(dirname(pathof(MixedModels)), "..", "docs"),
     sitename = "MixedModels",
-    pages = ["index.md",
-             "constructors.md",
-             "optimization.md",
-             "GaussHermite.md",
-             "bootstrap.md",
-            # "SimpleLMM.md",
-            # "MultipleTerms.md",
-            # "SingularCovariance.md",
-            # "SubjectItem.md",
-            # "benchmarks.md"
-        ],
-    )
+    pages = [
+        "index.md",
+        "constructors.md",
+        "optimization.md",
+        "GaussHermite.md",
+        "bootstrap.md",
+        # "SimpleLMM.md",
+        # "MultipleTerms.md",
+        # "SingularCovariance.md",
+        # "SubjectItem.md",
+        # "benchmarks.md"
+    ],
+)
 
-deploydocs(
-    repo = "github.com/JuliaStats/MixedModels.jl.git",
-    push_preview = true,
-    )
+deploydocs(repo = "github.com/JuliaStats/MixedModels.jl.git", push_preview = true)
