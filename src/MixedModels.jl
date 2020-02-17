d02b31347bd2fda172b61601d243588f8c29a231
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-02-17 14:24:49 +0100

    add docstring for MixedModel supertype that points to concrete terms (#272)

--- src/MixedModels.jl
+++ src/MixedModels.jl
@@ -109,6 +109,20 @@ export @formula,
 
 import Base: ==, *
 
+"""
+    MixedModel
+
+Abstract type for mixed models.  MixedModels.jl implements two subtypes:
+`LinearMixedModel` and `GeneralizedLinearMixedModel`.  See the documentation for
+each for more details.
+
+This type is primarily used for dispatch in `fit`.  Without a distribution and
+link function specified, a `LinearMixedModel` will be fit.  When a
+distribution/link function is provided, a `GeneralizedLinearModel` is fit,
+unless that distribution is `Normal` and the link is `IdentityLink`, in which
+case the resulting GLMM would be equivalent to a `LinearMixedModel` anyway and
+so the simpler, equivalent `LinearMixedModel` will be fit instead.
+"""
 abstract type MixedModel{T} <: StatsModels.RegressionModel end # model with fixed and random effects
 
 function __init__()
