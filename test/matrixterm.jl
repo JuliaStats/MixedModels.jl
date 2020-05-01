7378dc8fb964d6e01c1b366da547cf408683f7ca
Benedikt Ehinger <behinger@uos.de>
2020-05-01 09:11:05 +0200

    added very simple unittest. includes an example sparse array as used in unfold.jl

--- test/matrixterm.jl
+++ test/matrixterm.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, MixedModels, Random, Test
+using LinearAlgebra, MixedModels, Random, Test, SparseArrays
 
 @testset "femat" begin
     trm = MixedModels.FeMat(hcat(ones(30), repeat(0:9, outer = 3)), ["(Intercept)", "U"])
@@ -23,4 +23,53 @@ using LinearAlgebra, MixedModels, Random, Test
     @test size(trm') == (0, 10)
     @test eltype(trm) == Float64
     @test trm.rank == 0
+end
+
+@testset "fematSparse" begin
+    ## Generate a sparse design matrix
+    nrowsX = 50 # n events
+    ncolsX = 10 # n predictors
+    onsets = 5 .+ cumsum(Int.(round.((rand(MersenneTwister(1),nrowsX).*100)))) # minimal distance + random distance
+    ncolsBasis = 30 # expand each ncolsX by 30
+
+    X = rand(MersenneTwister(2),nrowsX,ncolsX) # generate predictors
+
+    # generate Diagonal Basis Expansion Matrices
+    basis = spdiagm(1 => 0.7*ones(ncolsBasis-1), 0 => 0.3*ones(ncolsBasis-1))
+    bases = repeat([basis],length(onsets))
+
+    # Generate sparse indices... for rows
+    rows =  copy.(rowvals.(bases))
+    for r in 1:length(rows)
+        rows[r] .+= onsets[r]-1
+    end
+    rows = vcat(rows...)
+    rows = repeat(rows,ncolsX)
+
+    # ... for cols
+    cols = []
+    for Xcol in 1:ncolsX
+        for b in 1:length(bases)
+            for c in 1:ncolsBasis
+                push!(cols,repeat([c+(Xcol-1)*ncolsBasis],length(nzrange(bases[b],c))))
+            end
+        end
+    end
+    cols = vcat(cols...)
+
+    # ... for values
+    vals = []
+    for Xcol in 1:ncolsX
+        push!(vals,vcat(nonzeros.(bases).*X[:,Xcol]...))
+    end
+    vals = vcat(vals...)
+
+    # Generate the matrix
+    Xdc = sparse(rows,cols,vals)
+
+    # Actual testing starts here
+    trm = MixedModels.FeMat(Xdc,string.(collect(range('a',length=10))))
+
+    @test typeof(trm.x) <: SparseMatrixCSC
+
 end
