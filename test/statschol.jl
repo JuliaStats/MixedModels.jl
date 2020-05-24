be3dfa5ba96a31e13679f0e35956c046ec3962f3
Phillip Alday <phillip.alday@mpi.nl>
2020-05-24 16:17:22 +0200

    don't forget zero

--- test/statschol.jl
+++ test/statschol.jl
@@ -8,7 +8,7 @@ const simdat = (
     U = repeat(0.:9, outer=20),
     V = repeat(-4.5:4.5, outer=20),
     Y = (1:200)/200,
-    Z = (-100:100)/200
+    Z = (-99:100)/200
 )
 
 @testset "fullranknumeric" begin
