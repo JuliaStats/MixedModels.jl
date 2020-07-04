3b5286976e6adb28ea9d084af377dea0e4a85098
Douglas Bates <dmbates@gmail.com>
2020-07-04 15:32:33 -0500

    Add a dims field to LinearMixedModel

--- src/linalg/logdet.jl
+++ src/linalg/logdet.jl
@@ -9,22 +9,10 @@ LD(d::Diagonal{T}) where {T<:Number} = sum(log, d.diag)
 
 function LD(d::UniformBlockDiagonal{T}) where {T}
     dat = d.data
-    m, n, k = size(dat)
-    m == n || throw(ArgumentError("Blocks of d must be square"))
-    s = log(one(T))
-    @inbounds for j = 1:k, i = 1:m
-        s += log(dat[i, i, j])
-    end
-    s
+    sum(log, dat[j, j, k] for j in axes(dat, 2), k in axes(dat, 3))
 end
 
-function LD(d::DenseMatrix{T}) where {T}
-    s = log(one(T))
-    for i = 1:LinearAlgebra.checksquare(d)
-        s += log(d[i, i])
-    end
-    s
-end
+LD(d::DenseMatrix{T}) where {T} = sum(log, d[k] for k in diagind(d))
 
 """
     logdet(m::LinearMixedModel)
@@ -36,15 +24,12 @@ Here LX is the diagonal term corresponding to the fixed-effects in the blocked
 lower Cholesky factor.
 """
 function LinearAlgebra.logdet(m::LinearMixedModel{T}) where {T}
-    s = log(one(T))
     L = m.L
-    nre = length(m.reterms)
-    @inbounds for i = 1:nre
-        s += LD(L[Block(i, i)])
-    end
+    nre = m.dims.nretrms
+    s = sum(LD, L[Block(j, j)] for j in Base.OneTo(nre))
     if m.optsum.REML
         feindex = nre + 1
         s += LD(L[Block(feindex, feindex)])
     end
-    2s
+    s + s
 end
