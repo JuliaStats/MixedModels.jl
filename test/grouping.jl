e46d27300de4f2d05abc33b94b233d5c198701b1
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-07-14 17:05:49 -0400

    remove tests that actually throw OOM

--- test/grouping.jl
+++ test/grouping.jl
@@ -3,7 +3,8 @@ using StatsModels
 
 @testset "Grouping pseudo-contrasts" begin
     d = (y = rand(2_000_000), grp=string.([1:1_000_000; 1:1_000_000]))
-    @test_throws OutOfMemoryError schema(d)
+    ## OOM seems to result in the process being killed on Mac so this messes up CI
+    # @test_throws OutOfMemoryError schema(d)
     sch = schema(d, Dict(:grp => Grouping()))
     t = sch[term(:grp)]
     @test t isa CategoricalTerm{Grouping}
@@ -15,6 +16,6 @@ using StatsModels
     @test all(t.contrasts.invindex[lev] == i for (i,lev) in enumerate(levs))
     @test all(t.contrasts.levels[i] == lev for (i,lev) in enumerate(levs))
 
-    @test_throws OutOfMemoryError fit(MixedModel, @formula(y ~ 1 + (1 | grp)), d)
+    # @test_throws OutOfMemoryError fit(MixedModel, @formula(y ~ 1 + (1 | grp)), d)
     @test fit(MixedModel, @formula(y ~ 1 + (1 | grp)), d, contrasts=Dict(:grp => Grouping())) isa LinearMixedModel
 end
