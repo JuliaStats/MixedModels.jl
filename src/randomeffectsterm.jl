7db06787f97a5ba13a3ae5b2c2de8e25f1534eea
Douglas Bates <dmbates@gmail.com>
2020-09-28 12:13:41 -0500

    Documentation for fulldummy (#385)
    * Documentation for fulldummy
    
    * update email address for @palday
    
    * fulldummy doc string warning on edge cases
    
    Co-authored-by: Phillip Alday <me@phillipalday.com>

--- src/randomeffectsterm.jl
+++ src/randomeffectsterm.jl
@@ -121,6 +121,19 @@ fulldummy(t::AbstractTerm) =
     throw(ArgumentError("can't promote $t (of type $(typeof(t))) to full dummy " *
                         "coding (only CategoricalTerms)"))
 
+"""
+    fulldummy(term::CategoricalTerm)
+
+Assign "contrasts" that include all indicator columns (dummy variables) and an intercept column.
+
+This will result in an under-determined set of contrasts, which is not a problem in the random
+effects because of the regularization, or "shrinkage", of the conditional modes.
+
+The interaction of `fulldummy` with complex random effects is subtle and complex with numerous 
+potential edge cases. As we discover these edge cases, we will document and determine their 
+behavior. Until such time, please check the model summary to verify that the expansion is 
+working as you expected. If it is not, please report a use case on GitHub.
+"""
 function fulldummy(t::CategoricalTerm)
     new_contrasts = StatsModels.ContrastsMatrix(
         StatsModels.FullDummyCoding(),
