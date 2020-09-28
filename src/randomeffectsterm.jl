43b2469814a58b940e3ae4b1571952ea7a5a26cb
Phillip Alday <me@phillipalday.com>
2020-09-28 17:50:19 +0200

    fulldummy doc string warning on edge cases

--- src/randomeffectsterm.jl
+++ src/randomeffectsterm.jl
@@ -128,6 +128,11 @@ Assign "contrasts" that include all indicator columns (dummy variables) and an i
 
 This will result in an under-determined set of contrasts, which is not a problem in the random
 effects because of the regularization, or "shrinkage", of the conditional modes.
+
+The interaction of `fulldummy` with complex random effects is subtle and complex with numerous 
+potential edge cases. As we discover these edge cases, we will document and determine their 
+behavior. Until such time, please check the model summary to verify that the expansion is 
+working as you expected. If it is not, please report a use case on GitHub.
 """
 function fulldummy(t::CategoricalTerm)
     new_contrasts = StatsModels.ContrastsMatrix(
