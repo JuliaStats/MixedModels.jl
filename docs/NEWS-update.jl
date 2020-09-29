6a7ce07c968fc5c64158c3074d1ad9d65da899de
Douglas Bates <dmbates@gmail.com>
2020-09-29 15:14:06 -0500

    Initial version of NEWS

---
+++ docs/NEWS-update.jl
@@ -0,0 +1,19 @@
+# Script to automatically insert Markdown footnotes for all [#xxxx] issue
+# cross-references in the NEWS file.
+
+NEWS = get(ARGS, 1, "NEWS.md")
+
+s = read(NEWS, String)
+
+m = match(r"\[#[0-9]+\]:", s)
+if m !== nothing
+    s = s[1:m.offset-1]
+end
+
+footnote(n) = "[#$n]: https://github.com/JuliaStats/MixedModels.jl/issues/$n"
+N = map(m -> parse(Int,m.captures[1]), eachmatch(r"\[#([0-9]+)\]", s))
+foots = join(map(footnote, sort!(unique(N))), "\n")
+
+open(NEWS, "w") do f
+    println(f, s, foots)
+end
