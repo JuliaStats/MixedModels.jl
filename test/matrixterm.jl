b8defc784269919b7954af20ce245a14b642a0b2
Benedikt Ehinger <behinger@uos.de>
2020-09-30 17:41:46 +0200

    SparseMatrixCSC support for fixed effects (#309)
    * Update femat.jl
    
    * added very simple unittest. includes an example sparse array as used in unfold.jl
    
    Co-authored-by: Phillip Alday <me@phillipalday.com>

--- test/matrixterm.jl
+++ test/matrixterm.jl
@@ -1,4 +1,6 @@
-using LinearAlgebra, MixedModels, Random, Test
+using LinearAlgebra, MixedModels, StableRNGs, Test, SparseArrays
+
+include("modelcache.jl")
 
 @testset "femat" begin
     trm = MixedModels.FeMat(hcat(ones(30), repeat(0:9, outer = 3)), ["(Intercept)", "U"])
@@ -12,7 +14,7 @@ using LinearAlgebra, MixedModels, Random, Test
     prd = trm'trm
     @test typeof(prd) == Matrix{Float64}
     @test prd == [30.0 135.0; 135.0 855.0][piv, piv]
-    wts = rand(MersenneTwister(123454321), 30)
+    wts = rand(StableRNG(123454321), 30)
     MixedModels.reweight!(trm, wts)
     @test mul!(prd, trm', trm)[ipiv[1], ipiv[1]] ≈ sum(abs2, wts)
 
@@ -23,4 +25,51 @@ using LinearAlgebra, MixedModels, Random, Test
     @test size(trm') == (0, 10)
     @test eltype(trm) == Float64
     @test trm.rank == 0
+end
+
+@testset "fematSparse" begin
+
+    @testset "sparse and dense yield same fit" begin
+        # deepcopy because we're going to modify
+        m = deepcopy(last(models(:insteval)))
+        # this is kinda sparse: 
+        # julia> mean(first(m.feterms).x)
+        # 0.10040140325753434
+        
+        fe = first(m.feterms)
+        X =  MixedModels.FeMat(SparseMatrixCSC(fe.x), fe.cnames)
+        @test typeof(X.x) <: SparseMatrixCSC
+        @test typeof(X.wtx) <: SparseMatrixCSC
+        @test X.rank == 28
+        @test X.cnames == fe.cnames
+
+        dense_θ = copy(m.θ)
+        dense_feval = m.optsum.feval
+        m.feterms[1] = X    
+        refit!(m)
+
+        @test dense_θ ≈ m.θ
+    end
+
+    @testset "rank defiency in sparse FeMat" begin
+        trm = MixedModels.FeMat(SparseMatrixCSC(hcat(ones(30), 
+                                                     repeat(0:9, outer = 3), 
+                                                     2repeat(0:9, outer = 3))), 
+                                ["(Intercept)", "U", "V"])
+        piv = trm.piv
+        ipiv = invperm(piv)
+        @test_broken rank(trm) == 2
+        @test size(trm) == (30, 3)
+        @test length(trm) == 90
+        @test size(trm') == (3, 30)
+        @test eltype(trm) == Float64
+        @test trm.x === trm.wtx
+        prd = trm'trm
+        @test typeof(prd) == typeof(prd)
+        @test prd == [30.0 135.0 270.0; 135.0 855.0 1710.0; 270.0 1710.0 3420][piv, piv]
+        wts = rand(StableRNG(123454321), 30)
+        MixedModels.reweight!(trm, wts)
+        @test mul!(prd, trm', trm)[ipiv[1], ipiv[1]] ≈ sum(abs2, wts)
+    end
+    
 end
