1a740cf5600fa2e31958d50003becc0ee1cd3213
Phillip Alday <palday@users.noreply.github.com>
2021-02-17 21:06:06 +0100

    remove (unused) `named` kwarg from ranef (#469)
    (cherry picked from commit e14078a8ccf6c01618a3b61607e7448b10fc6332)

--- src/linearmixedmodel.jl
+++ src/linearmixedmodel.jl
@@ -689,14 +689,16 @@ end
 ranef!(v::Vector, m::LinearMixedModel, uscale::Bool) = ranef!(v, m, fixef(m), uscale)
 
 """
-    ranef(m::LinearMixedModel; uscale=false, named=false)
+    ranef(m::LinearMixedModel; uscale=false)
 
 Return, as a `Vector{Matrix{T}}`, the conditional modes of the random effects in model `m`.
 
 If `uscale` is `true` the random effects are on the spherical (i.e. `u`) scale, otherwise on
 the original scale.
+
+For a named variant, see [`@raneftables`](@ref).
 """
-function ranef(m::LinearMixedModel{T}; uscale = false, named = false) where {T}
+function ranef(m::LinearMixedModel{T}; uscale = false) where {T}
     reterms = m.reterms
     v = [Matrix{T}(undef, size(t.z, 1), nlevs(t)) for t in reterms]
     ranef!(v, m, uscale)
