239905c17f1bc05c0b15bb1af62116b0cd814757
Phillip Alday <me@phillipalday.com>
2021-02-26 12:12:42 +0100

    fix future guard

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ import LinearAlgebra: BLAS
 # there seem to be processor-specific issues and knowing this is helpful
 println(versioninfo())
 @show BLAS.vendor()
-@static if VERSION > v"1.7.0-DEV.610"
+@static if VERSION < v"1.7.0-DEV.610"
     # this has gone away but it would be good to still
     # get a lot of BLAS info for debugging purposes
     if startswith(string(BLAS.vendor()), "openblas")
