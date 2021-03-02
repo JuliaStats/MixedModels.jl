92e75b50c40b5e38fb3688ee2ccc4738112c6a90
Douglas Bates <dmbates@gmail.com>
2021-03-02 16:37:29 -0600

    Update src/linalg/pivot.jl
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/linalg/pivot.jl
+++ src/linalg/pivot.jl
@@ -27,7 +27,7 @@ function statsrank(x::AbstractMatrix{T}; ranktol=1e-8) where {T<:AbstractFloat}
         v1 .*= (fdv + true) / sqrt(m)
         qrpiv = qr(x, Val(true))
         piv = qrpiv.p
-        fill!(v1, true)    # restore the contents of the first column
+        fill!(v1, one(T))    # restore the contents of the first column
     end
 
     # preserve as much of the original column ordering as possible
