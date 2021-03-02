a3bfcc7ffed1dce956770fe3c90ec7150a8bfc59
Douglas Bates <dmbates@gmail.com>
2021-03-02 15:33:12 -0600

    statsrank just returning rank and pivot

--- src/linalg/pivot.jl
+++ src/linalg/pivot.jl
@@ -1,35 +1,36 @@
 """
-    statsqr(x::Matrix{T}, ranktol::Real=1e-8) where {T<:AbstractFloat}
+    statsrank(x::Matrix{T}, ranktol::Real=1e-8) where {T<:AbstractFloat}
 
-Return the numerical column rank and, for a rank-deficient X, a pivot vector
+Return the numerical column rank and a pivot vector.
+
+The rank is determined from the absolute values of the diagonal of R from
+a pivoted QR decomposition, relative to the first (and, hence, largest) 
+element of this vector.
+
+In the full-rank case the pivot vector is `collect(axes(x, 2))`.
 """
-function statsqr(x::Matrix{T}; ranktol=1e-8) where {T<:AbstractFloat}
-    n = size(x, 2)
+function statsrank(x::AbstractMatrix{T}; ranktol=1e-8) where {T<:AbstractFloat}
+    m, n = size(x)
 
     qrpiv = qr(x, Val(true))
-    rank =  searchsortedlast(abs.(diag(qrpiv.R)), ranktol, rev=true);
-    if rank < n
-        piv = qrpiv.p
-    else
-        piv = LinearAlgebra.BlasInt.(collect(1:n))
+    dvec = abs.(diag(qrpiv.R))
+    fdv = first(dvec)
+    if (last(dvec) / fdv) > ranktol
+        return (rank = n, piv = collect(axes(x, 2)))
     end
-
-    if rank < n && piv[1] ≠ 1
-        # make sure the first column isn't moved
-        # this is usually the intercept and it's desirable to avoid
-        # pivoting the intercept out. note that this inflation may
-        # not always be enough, but if it isn't, then you probably
-        # have other problems
-
-        x = deepcopy(x) # this is horrible
-        x[:, 1] .*= 1e6
+    rank = searchsortedlast(dvec ./ fdv, ranktol, rev=true)
+    @assert rank < n
+    piv = qrpiv.p
+    v1 = first(eachcol(x))
+    if all(isone, v1) && first(piv) ≠ 1
+        # make sure the first column isn't moved by inflating v1
+        v1 .*= (fdv + true) / sqrt(m)
         qrpiv = qr(x, Val(true))
         piv = qrpiv.p
+        fill!(v1, true)    # restore the contents of the first column
     end
 
     # preserve as much of the original column ordering as possible
-    piv = [sort!(piv[1:rank]); sort!(piv[rank+1:n])];
-    qrunp = qr(x[:, piv], Val(false));
-
-    QRPivoted(qrunp.factors, qrpiv.τ, piv)
+    sort!(view(piv, 1:rank))
+    (rank = rank, piv = piv)
 end
--- test/pivot.jl
+++ test/pivot.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra, StableRNGs, StatsModels, Test
 
-import MixedModels: statsqr, statscholesky
+import MixedModels: statsrank
 
 xtx(X) = Symmetric(X'X, :U)  # creat the symmetric matrix X'X from X
 LinearAlgebra.rank(F::LinearAlgebra.QRPivoted; tol=1e-8) = searchsortedlast(abs.(diag(F.R)), tol, rev=true);
@@ -18,32 +18,32 @@ const simdat = (
 
 @testset "fullranknumeric" begin
     mm = modelmatrix(@formula(Y ~ 1 + U), simdat)
-    st = statsqr(mm)
-    @test st.p == 1:2
+    r, pivot = statsrank(mm)
+    @test pivot == 1:2
 end
 
 @testset "fullrankcategorical" begin
     mm = modelmatrix(@formula(Y ~ 1 + G*H), simdat)
-    st = statsqr(mm)
-    @test rank(st) == 100
-    @test st.p == 1:100
+    r, pivot = statsrank(mm)
+    @test r == 100
+    @test pivot == 1:100
 end
 
 @testset "dependentcolumn" begin
     mm = modelmatrix(@formula(Y ~ 1 + U + V + Z), simdat)
-    st = statsqr(mm)
+    r, pivot = statsrank(mm)
     perm = [1,2,4,3]
-    @test rank(st) == 3
-    @test st.p == perm
+    @test r == 3
+    @test pivot == perm
 end
 
 @testset "qr missing cells" begin
     mm = modelmatrix(@formula(Y ~ 1 + G*H), simdat)[5:end,:]
-    st = statsqr(mm)
-    @test rank(st) == 98
+    r, pivot = statsrank(mm)
+    @test r == 98
     # we no longer offer ordering guarantees besides preserving
     # relative order of linearly independent columns
     # and trying to keep the first column in the first position
-    unpivoted = st.p[begin:rank(st)]
+    unpivoted = pivot[begin:r]
     @test unpivoted == sort(unpivoted)
 end
