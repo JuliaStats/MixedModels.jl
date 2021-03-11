135e87d09ed5adfbbb3fe7eea1c7a8c6771b5a2c
Phillip Alday <palday@users.noreply.github.com>
2021-03-11 23:39:06 +0000

    bring back LazyArtifacts (#486)
    * bring back LazyArtifacts
    
    * drop Pkg dep
    
    * compat entry for lazyartifacts (not stdlib on julia <1.6)

--- src/datasets.jl
+++ src/datasets.jl
@@ -1,3 +1,6 @@
+_testdata() = artifact"TestData"
+
+
 cacheddatasets = Dict{String, Arrow.Table}()
 """
     dataset(nm)
@@ -6,7 +9,7 @@ Return, as an `Arrow.Table`, the test data set named `nm`, which can be a `Strin
 """
 function dataset(nm::AbstractString)
     get!(cacheddatasets, nm) do
-        path = joinpath(TestData, nm * ".arrow")
+        path = joinpath(_testdata(), nm * ".arrow")
         if !isfile(path)
             throw(ArgumentError(
                 "Dataset \"$nm\" is not available.\nUse MixedModels.datasets() for available names."))
@@ -21,4 +24,4 @@ dataset(nm::Symbol) = dataset(string(nm))
 
 Return a vector of names of the available test data sets
 """
-datasets() = first.(Base.Filesystem.splitext.(filter(endswith(".arrow"), readdir(TestData))))
+datasets() = first.(Base.Filesystem.splitext.(filter(endswith(".arrow"), readdir(_testdata()))))
