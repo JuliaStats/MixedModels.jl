4874636454c85725c06951f2a40eebfaed2cb7d5
Phillip Alday <me@phillipalday.com>
2021-06-29 17:41:29 -0500

    whitespace trim

--- src/mixedmodel.jl
+++ src/mixedmodel.jl
@@ -18,20 +18,20 @@ end
     dof_residual(m::MixedModel)
 
   Return the residual degrees of freedom of the model.
-    
+
 !!! note
-  The residual degrees of freedom for mixed-effects models is not clearly defined due to partial pooling. 
-  The classical `nobs(m) - dof(m)` fails to capture the extra freedom granted by the random effects, but 
-  `nobs(m) - nranef(m)` would overestimate the freedom granted by the random effects. `nobs(m) - sum(leverage(m))` 
-  provides a nice balance based on the relative influence of each observation, but is computationally 
-  expensive for large models. This problem is also fundamentally related to [long-standing debates](https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#why-doesnt-lme4-display-denominator-degrees-of-freedomp-values-what-other-options-do-i-have) 
+  The residual degrees of freedom for mixed-effects models is not clearly defined due to partial pooling.
+  The classical `nobs(m) - dof(m)` fails to capture the extra freedom granted by the random effects, but
+  `nobs(m) - nranef(m)` would overestimate the freedom granted by the random effects. `nobs(m) - sum(leverage(m))`
+  provides a nice balance based on the relative influence of each observation, but is computationally
+  expensive for large models. This problem is also fundamentally related to [long-standing debates](https://bbolker.github.io/mixedmodels-misc/glmmFAQ.html#why-doesnt-lme4-display-denominator-degrees-of-freedomp-values-what-other-options-do-i-have)
   about the appropriate treatment of the denominator degrees of freedom for ``F``-tests.
-  In the future, MixedModels.jl may provide additional methods allowing the user to choose the computation 
+  In the future, MixedModels.jl may provide additional methods allowing the user to choose the computation
   to use.
 
 !!! warning
-   Currently, the residual degrees of freedom is computed as `nobs(m) - dof(m)`, but this may change in 
-   the future without being considered a breaking change because there is no canonical definition of the 
+   Currently, the residual degrees of freedom is computed as `nobs(m) - dof(m)`, but this may change in
+   the future without being considered a breaking change because there is no canonical definition of the
    residual degrees of freedom in a mixed-effects model.
 """
 function StatsBase.dof_residual(m::MixedModel)
@@ -95,7 +95,7 @@ Return the response vector for the model.
 For a linear mixed model this is a `view` of the last column of the `XyMat` field.
 For a generalized linear mixed model this is the `m.resp.y` field.
 In either case it should be copied if it is to be modified.
-""" 
+"""
 StatsBase.response(m::MixedModel) = m.y
 
 function StatsBase.responsename(m::MixedModel)
