18fe00f6ba8433fe8b0f928b8e49b56e1f1943bc
Phillip Alday <me@phillipalday.com>
2021-08-06 09:02:57 -0500

    tinkering with example

--- docs/src/multimembership.jl
+++ docs/src/multimembership.jl
@@ -1,4 +1,3 @@
-using CairoMakie
 using DataFrames
 using Distributions
 using LinearAlgebra
@@ -15,13 +14,15 @@ nm = 20
 nobs0 = 500
 const RNG = MersenneTwister(42)
 W = rand(RNG, Bernoulli(0.25), nobs0, nm)
-b = randn(RNG, nm)
+b = randn(RNG, nm) * 100
 beta = [1, 2]
 x = rand(RNG, nobs0)
 X = [ones(size(x,1)) x]
 y = X * beta + W * b + randn(RNG, nobs0)
-df = DataFrame(; x, y, fake=repeat('A':('A'+nm-1); outer=(nobs0 ÷ nm)))
-
+df = DataFrame(; x, y, fake=string.(repeat('A':('A'+nm-1); outer=(nobs0 ÷ nm))))
+# Wf = float.(W)
+# Wf ./= sum(eachcol(Wf))
+# Wf[isnan.(Wf)] .= 0
 
 mtemp = LinearMixedModel(@formula(y ~ 1 + x + (1|fake)), df; contrasts=Dict(:fake => Grouping()))
 rtemp = mtemp.reterms[1]
@@ -35,8 +36,9 @@ reterm = MultimembershipReMat{Float64,1}(rtemp.trm,
                               deepcopy(rtemp.inds),
                               SparseMatrixCSC{Float64, Int32}(W'),
                               deepcopy(rtemp.scratch))
-# copyto!(reterm.adjA, SparseMatrixCSC{Float64, Int32}(W'))
 model = LinearMixedModel(df.y, mtemp.feterm, AbstractReMat{Float64}[reterm], mtemp.formula)
+model.optsum.initial_step .= [0.01]
+fit!(model)
 BlockDescription(mtemp)
 BlockDescription(model)
 
