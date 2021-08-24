526dd955554893cd0ac92bfd6844715bb04fba25
Douglas Bates <dmbates@gmail.com>
2021-08-24 12:24:31 -0500

    Update src/linearmixedmodel.jl
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/linearmixedmodel.jl
+++ src/linearmixedmodel.jl
@@ -1001,8 +1001,8 @@ Return the lower Cholesky factor `L` as a `SparseMatrix{T,Int32}`.
 `full` indicates whether the parts of `L` associated with the fixed-effects and response
 are to be included.
 
-When `fname` is the name of a grouping factor the blocks to the left of that block are
-dropped.
+`fname` specifies the first grouping factor to include. Blocks to the left of the block corresponding
+ to `fname` are dropped. The default is the first, i.e., leftmost block and hence all blocks.
 """
 function sparseL(
     m::LinearMixedModel{T};
