3dbbb85df80d23ec1a1640a2bf32e399fb77a1d7
Phillip Alday <phillip.alday@beacon.bio>
2022-10-05 15:43:18 -0500

    LTS test compat

--- test/misc.jl
+++ test/misc.jl
@@ -8,11 +8,11 @@ using MixedModels: dataset
 
     @test MixedModel(@formula(yield ~ 0 + (1|batch)), dyestuff) isa LinearMixedModel
     @test MixedModel(@formula(yield ~ 1 + (1|batch)), dyestuff) isa LinearMixedModel
-    @test_throws r"Formula contains no random effects"  MixedModel(@formula(yield ~ 0 + batch), dyestuff)
-    @test_throws r"Formula contains no random effects"  MixedModel(@formula(yield ~ 1), dyestuff)
+    @test_throws MixedModels._MISSING_RE_ERROR  MixedModel(@formula(yield ~ 0 + batch), dyestuff)
+    @test_throws MixedModels._MISSING_RE_ERROR MixedModel(@formula(yield ~ 1), dyestuff)
 
     @test MixedModel(@formula(yield ~ 0 + (1|batch)), dyestuff, Poisson()) isa GeneralizedLinearMixedModel
     @test MixedModel(@formula(yield ~ 1 + (1|batch)), dyestuff, Poisson()) isa GeneralizedLinearMixedModel
-    @test_throws r"Formula contains no random effects"  MixedModel(@formula(yield ~ 0 + batch), dyestuff, Poisson())
-    @test_throws r"Formula contains no random effects"  MixedModel(@formula(yield ~ 1), dyestuff, Poisson())
+    @test_throws MixedModels._MISSING_RE_ERROR  MixedModel(@formula(yield ~ 0 + batch), dyestuff, Poisson())
+    @test_throws MixedModels._MISSING_RE_ERROR  MixedModel(@formula(yield ~ 1), dyestuff, Poisson())
 end
