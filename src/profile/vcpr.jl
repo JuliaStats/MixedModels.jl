8bf48e9268e995e5c5e76beae604ae1ee32d91fd
Douglas Bates <dmbates@gmail.com>
2023-01-28 11:14:37 -0600

    Fix σ profiles by moving lowerbd on θ

--- src/profile/vcpr.jl
+++ src/profile/vcpr.jl
@@ -4,49 +4,52 @@ function profilevc(m::LinearMixedModel{T}, val::T, rowj::AbstractVector{T}) wher
     function obj(x, g)
         isempty(g) || throw(ArgumentError("g must be empty"))
         updateL!(setθ!(m, x))
-        optsum.sigma = val / norm(rowj) 
-        return objective(m)
+        optsum.sigma = val / norm(rowj)
+        objctv = objective(m)
+        return objctv
     end
     opt = Opt(optsum)
     NLopt.min_objective!(opt, obj)
     fmin, xmin, ret = NLopt.optimize!(opt, copyto!(optsum.final, optsum.initial))
     _check_nlopt_return(ret)
-    return fmin
+    return fmin, xmin
 end
 
-function profileσs!(pr::MixedModelProfile{T}, tc::TableColumns{T}) where {T}
+function profileσs!(pr::MixedModelProfile{T}, tc::TableColumns{T}; nzlb=1.0e-8) where {T}
     m = pr.m
     @compat (; λ, σ, β, optsum, parmap, reterms) = m
     isnothing(optsum.sigma) || throw(ArgumentError("Can't profile vc's when σ is fixed"))
-    @compat (; initial, final, fmin) = optsum
+    @compat (; initial, final, fmin, lowerbd) = optsum
+    lowerbd .+= T(nzlb)                       # lower bounds must be > 0 b/c θ's occur in denominators
     saveinitial = copy(initial)
-    copyto!(initial, final)
-    zetazero = mkrow!(tc, m, zero(T)) # parameter estimates
+    copyto!(initial, max.(final, lowerbd))
+    zetazero = mkrow!(tc, m, zero(T))         # parameter estimates
     vcnms = filter(sym -> (str = string(sym); startswith(str, 'σ') && length(str) > 1), keys(first(pr.tbl)))
     ind = 0
     for t in reterms
         for r in eachrow(t.λ.data)
-            optsum.sigma = nothing        # re-initialize the model
+            optsum.sigma = nothing            # re-initialize the model
             objective!(m, final)
             ind += 1
             sym = vcnms[ind]
-            gpsym = getproperty(sym)      # extractor function
+            gpsym = getproperty(sym)          # extractor function
             estimate = gpsym(zetazero)
             pnm = (; p = sym,)
             tbl = [merge(pnm, zetazero)]
             xtrms = extrema(gpsym, pr.tbl)
             lub = log(last(xtrms))
             llb = log(max(first(xtrms), T(0.01) * lub))
-            for lx in LinRange(llb, lub, 15)
+            for lx in LinRange(lub, llb, 15)  # start at the upper bound where things are more stable
                 x = exp(lx)
-                obj = profilevc(m, x, r)
+                obj, xmin = profilevc(m, x, r)
+                copyto!(initial, xmin)
                 zeta = sign(x - estimate) * sqrt(max(zero(T), obj - fmin))
                 push!(tbl, merge(pnm, mkrow!(tc, m, zeta)))
             end
-            if iszero(first(xtrms)) && !iszero(estimate)      # handle the case of lower bound of zero
+            if iszero(first(xtrms)) && !iszero(estimate) # handle the case of lower bound of zero
                 zrows = filter(iszero ∘ gpsym, pr.tbl)
                 isone(length(zrows)) || filter!(r -> iszero(getproperty(r, first(r))), zrows)
-                rr = only(zrows)          # will error if zeros in sym column occur in unexpected places
+                rr = only(zrows)              # will error if zeros in sym column occur in unexpected places
                 push!(tbl, merge(pnm, rr[(collect(keys(rr))[2:end]...,)]))
             end
             sort!(tbl; by = gpsym)
@@ -59,6 +62,7 @@ function profileσs!(pr::MixedModelProfile{T}, tc::TableColumns{T}) where {T}
     end
     copyto!(final, initial)
     copyto!(initial, saveinitial)
+    lowerbd .-= T(nzlb)
     optsum.sigma = nothing
     updateL!(setθ!(m, final))
     return pr
