8a4ab2c61f968566a2570e2d4f347e031bcd66dc
Douglas Bates <dmbates@gmail.com>
2023-03-20 14:58:06 -0500

    Update src/linearmixedmodel.jl
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/linearmixedmodel.jl
+++ src/linearmixedmodel.jl
@@ -807,10 +807,13 @@ end
 
 """
     objective!(m::LinearMixedModel, θ)
+    objective!(m::LinearMixedModel)
 
 Equivalent to `objective(updateL!(setθ!(m, θ)))`.
 
-When `m` has a single, scalar random-effects term, `θ` can be a scalar
+When `m` has a single, scalar random-effects term, `θ` can be a scalar.
+
+The one-argument method curries and returns a single-argument function of `θ`.
 
 Note that these methods modify `m`.
 The calling function is responsible for restoring the optimal `θ`.
