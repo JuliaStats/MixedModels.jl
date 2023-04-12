855724f99196534c3bab28d3d9b7161296ccaa75
Douglas Bates <dmbates@gmail.com>
2023-04-12 11:45:07 -0500

    Update src/mixedmodel.jl
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/mixedmodel.jl
+++ src/mixedmodel.jl
@@ -100,7 +100,10 @@ end
 """
     raneftables(m::MixedModel; uscale = false)
 
-Return the conditional means of the random effects as a `NamedTuple` of `DictTable`s
+Return the conditional means of the random effects as a `NamedTuple` of Tables.jl-compliant tables.
+	
+!!! note
+    The API guarantee is only that the NamedTuple contains Tables.jl tables and not on the particular concrete type of each table.
 """
 function raneftables(m::MixedModel{T}; uscale=false) where {T}
     return NamedTuple{fnames(m)}((map(retbl, ranef(m; uscale=uscale), m.reterms)...,))
