2a24f5560da18fd65fcf9be0e793a5f092a46359
Phillip Alday <me@phillipalday.com>
2023-04-11 21:02:23 -0500

    remove pre 1.7 codepath

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,7 @@ import LinearAlgebra: BLAS
 
 # there seem to be processor-specific issues and knowing this is helpful
 println(versioninfo())
-@static if VERSION ≥ v"1.7.0-DEV.620"
-    println(BLAS.get_config())
-else
-    @show BLAS.vendor()
-    if startswith(string(BLAS.vendor()), "openblas")
-        println(BLAS.openblas_get_config())
-    end
-end
+println(BLAS.get_config())
 
 @testset "Aqua" begin
     # we can't check for unbound type parameters
