0c3904be9325836e8bc859e43842892df67a548f
Douglas Bates <dmbates@gmail.com>
2023-05-04 10:22:09 -0500

    Document a method src/profile/sigmapr.jl as volatile
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/profile/sigmapr.jl
+++ src/profile/sigmapr.jl
@@ -33,6 +33,10 @@ end
     profileσ(m::LinearMixedModel, tc::TableColumns; threshold=4)
 
 Return a Table of the profile of `σ` for model `m`.  The profile extends to where the magnitude of ζ exceeds `threshold`.
+!!! note
+   This method is called by `profile` and currently considered internal.
+   As such, it may change or disappear in a future release without being considered breaking.
+   
 """
 function profileσ(m::LinearMixedModel{T}, tc::TableColumns{T}; threshold=4) where {T}
     (; σ, optsum) = m
