c381204ba5ea8b2ab2b7120b861031d5b2534faf
Douglas Bates <dmbates@gmail.com>
2023-05-04 10:30:44 -0500

    Document profilevcl
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/profile/vcpr.jl
+++ src/profile/vcpr.jl
@@ -1,4 +1,13 @@
 
+"""
+     profilevc(m::LinearMixedModel{T}, val::T, rowj::AbstractVector{T}) where {T}
+
+Profile an element of the variance components.
+
+!!! note
+   This method is called by `profile` and currently considered internal.
+   As such, it may change or disappear in a future release without being considered breaking.
+"""
 function profilevc(m::LinearMixedModel{T}, val::T, rowj::AbstractVector{T}) where {T}
     optsum = m.optsum
     function obj(x, g)
