3a6baebd9d7e32ae8ae97198c210c5b84b4a5344
Douglas Bates <dmbates@gmail.com>
2023-05-04 10:30:00 -0500

    Document confint method
    Co-authored-by: Phillip Alday <palday@users.noreply.github.com>

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -165,6 +165,23 @@ function allpars(bsamp::MixedModelFitCollection{T}) where {T}
     )
 end
 
+"""
+    confint(pr::MixedModelBootstrap; level::Real=0.95)
+    
+Compute bootstrap confidence intervals for coefficients and variance components, with confidence level level (by default 95%).
+
+!!! note
+    The API guarantee is for a Tables.jl compatible table. The exact return type is an 
+    implementation detail and may change in a future minor release without being considered
+    breaking.
+    
+!!! note 
+   The "row names" indicating the associated parameter name are guaranteed to be unambiguous,
+   but their precise naming scheme is not yet stable and may change in a future release
+   without being considered breaking.
+
+See also [`shortestcovint`](@ref).
+"""
 function StatsBase.confint(bsamp::MixedModelBootstrap{T}; level::Real=0.95) where {T}
     cutoff = sqrt(quantile(Chisq(1), level))
     # Creating the table is somewhat wasteful because columns are created then immediately skipped.
