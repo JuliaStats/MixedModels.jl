3886f7adbbee1271fe5482b2352ba391a1a15c12
Phillip Alday <palday@users.noreply.github.com>
2023-08-08 12:15:39 -0500

    Update src/serialization.jl
    Co-authored-by: Douglas Bates <dmbates@gmail.com>

--- src/serialization.jl
+++ src/serialization.jl
@@ -5,7 +5,7 @@
 Read, check, and restore the `optsum` field from a JSON stream or filename.
 """
 function restoreoptsum!(
-    m::LinearMixedModel{T}, io::IO; atol::Real=0, rtol::Real=atol > 0 ? 0 : √eps(T)
+    m::LinearMixedModel{T}, io::IO; atol::Real=zero(T), rtol::Real=atol > 0 ? zero(T) : √eps(T)
 ) where {T}
     dict = JSON3.read(io)
     ops = m.optsum
