23a2d24720921182f2d32085fde3f3468f085481
Phillip Alday <phillip.alday@beacon.bio>
2023-08-22 22:25:50 -0500

    use median

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -1,8 +1,8 @@
 using Pkg
 Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
-Pkg.update()
-using PkgBenchmark, MixedModels
+Pkg.instantiate()
+using PkgBenchmark, MixedModels, Statistics
 # Pkg.update() allows us to benchmark even when dependencies/compat requirements change
 juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update()"`
 config = BenchmarkConfig(; id="origin/HEAD", juliacmd)
-export_markdown("benchmark.md", judge(MixedModels, config; verbose=true))
+export_markdown("benchmark.md", judge(MixedModels, config; verbose=true, retune=true, f=median))
