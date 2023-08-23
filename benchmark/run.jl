023eba5b6ad9795d289ee20b6f8790994a014f07
Phillip Alday <phillip.alday@beacon.bio>
2023-08-22 23:45:46 -0500

    d'oh

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -2,12 +2,12 @@ using Pkg
 Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
 Pkg.instantiate()
 using PkgBenchmark, MixedModels, StatsBase
-using BenchmarkTools: BenchmarkGroup
+using BenchmarkTools: BenchmarkGroup, mapvals
 # Pkg.update() allows us to benchmark even when dependencies/compat requirements change
 juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update()"`
 config = BenchmarkConfig(; id="origin/HEAD", juliacmd)
 # for many of the smaller models, we get a lot of noise at the default 5% tolerance
 # TODO: specify a tune.json with per model time tolerances
-StatsBase.trim(group::BenchmarkGroup; kwargs...) = PkgBenchmark.mapvals(x -> trim(x; kwargs...), group)
+StatsBase.trim(group::BenchmarkGroup; kwargs...) = mapvals(x -> trim(x; kwargs...), group)
 robustify(f) = x -> f(trim(x; prop=0.10))
 export_markdown("benchmark.md", judge(MixedModels, config; verbose=true, retune=false, f=robustify(maximum), judgekwargs=(;time_tolerance=0.1)))
