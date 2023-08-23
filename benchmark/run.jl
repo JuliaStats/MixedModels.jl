01a1bf6867a2a2e50d364b5579e13aeede13c80e
Phillip Alday <phillip.alday@beacon.bio>
2023-08-22 22:43:01 -0500

    increase time tolerance

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -5,4 +5,6 @@ using PkgBenchmark, MixedModels, Statistics
 # Pkg.update() allows us to benchmark even when dependencies/compat requirements change
 juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update()"`
 config = BenchmarkConfig(; id="origin/HEAD", juliacmd)
-export_markdown("benchmark.md", judge(MixedModels, config; verbose=true, retune=false, f=median))
+# for many of the smaller models, we get a lot of noise at the default 5% tolerance
+# TODO: specify a tune.json with per model time tolerances
+export_markdown("benchmark.md", judge(MixedModels, config; verbose=true, retune=false, f=median, judgekwargs=(;time_tolerance=0.1)))
