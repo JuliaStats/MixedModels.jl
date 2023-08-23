f9ed3f1a54d005f4b9b68ddc0b519d21c51ef21f
Phillip Alday <me@phillipalday.com>
2023-08-22 20:29:58 -0500

    remove Pkg.add from Benchmark

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -2,8 +2,7 @@ using Pkg
 Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
 Pkg.instantiate()
 using PkgBenchmark, MixedModels
-# explicit `Pkg.add` is a crutch until we've got a good base on main
 # Pkg.update() allows us to benchmark even when dependencies/compat requirements change
-juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update(); Pkg.add([\"BenchmarkTools\", \"StatsModels\"])"`
+juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update()"`
 config = BenchmarkConfig(; id="origin/HEAD", juliacmd)
 export_markdown("benchmark.md", judge(MixedModels, config; verbose=true))
