13c00815b8ee911f0a6f302064d8cceed59e714f
Phillip Alday <me@phillipalday.com>
2023-09-12 02:12:49 -0500

    more docs

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -323,6 +323,10 @@ end
 
 Compute bootstrap confidence intervals for coefficients and variance components, with confidence level level (by default 95%).
 
+The keyword argument `method` determines whether the `:shortest`, i.e. highest density, interval is used 
+or the `:equaltail`, i.e. quantile-based, interval is used. For historical reasons, the default is `:shortest`, 
+but `:equaltail` gives the interval that is most comparable to the profile and Wald confidence intervals.
+
 !!! note
     The API guarantee is for a Tables.jl compatible table. The exact return type is an
     implementation detail and may change in a future minor release without being considered
