1df410f6dc6b6cbca03560495ac4e82589f5bc63
Douglas Bates <dmbates@gmail.com>
2024-02-05 13:45:20 -0600

    Use DataFrames to display the table.

--- test/PRIMA.jl
+++ test/PRIMA.jl
@@ -1,7 +1,7 @@
 # This test file is not run with the package tests.
 # Compare the PRIMA and NLopt implementations of BOBYQA.
 
-using MixedModels, PRIMA
+using DataFrames, MixedModels, PRIMA
 
 include("modelcache.jl")
 @isdefined(contrasts) || const contrasts = Dict{Symbol,Any}()
@@ -12,24 +12,31 @@ contrasts[:service] = HelmertCoding()
 
 
 function comparePRIMA(dsnm)
-    res = @NamedTuple{ds::Symbol, Pfinal::Float64, Nfinal::Float64, Peval::Int64, Neval::Int64}[]
+    res = @NamedTuple{ds::Symbol, frm::Int, Pfinal::Float64, fdiff::Float64, Peval::Int, ediff::Int}[]
     if haskey(fms, dsnm)
-        for f in fms[dsnm]
+        for (i, f) in enumerate(fms[dsnm])
             m = LinearMixedModel(f, dataset(dsnm); contrasts)
             x, info = bobyqa(objective!(m), m.optsum.initial; xl=m.optsum.lowerbd)
             fit!(m; progress=false)
             push!(
                 res,
                 (;
-                    ds = dsnm,
+                    ds=dsnm,
+                    frm=i,
                     Pfinal=info.fx,
-                    Nfinal=m.optsum.fmin,
+                    fdiff=m.optsum.fmin - info.fx,
                     Peval=info.nf,
-                    Neval=m.optsum.feval,
+                    ediff=m.optsum.feval - info.nf,
                 )
             )
         end
     end
-    return 
+    return res
 end
 
+res = comparePRIMA(:dyestuff2)
+for k in keys(fms)
+    k == :dyestuff2 || append!(res, comparePRIMA(k))
+end
+
+println(DataFrame(sort(res, by=getproperty(:ediff))))
