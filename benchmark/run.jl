cfd302310ba66ad5008e97738bd6710cf5a7509a
Phillip Alday <palday@users.noreply.github.com>
2024-03-05 16:18:20 -0600

    remove Pkg.add from Benchmark (#711)
    * remove Pkg.add from Benchmark
    
    * update
    
    * use median
    
    * disable retune
    
    * increase time tolerance
    
    * robustify
    
    * try forcing tuning
    
    * only run benchmark ci manually

--- benchmark/run.jl
+++ benchmark/run.jl
@@ -1,9 +1,10 @@
 using Pkg
 Pkg.develop(PackageSpec(path=dirname(@__DIR__)))
 Pkg.instantiate()
-using PkgBenchmark, MixedModels
-# explicit `Pkg.add` is a crutch until we've got a good base on main
+using PkgBenchmark, MixedModels, Statistics
 # Pkg.update() allows us to benchmark even when dependencies/compat requirements change
-juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update(); Pkg.add([\"BenchmarkTools\", \"StatsModels\"])"`
+juliacmd = `$(Base.julia_cmd()) -O3 -e "using Pkg; Pkg.update()"`
 config = BenchmarkConfig(; id="origin/HEAD", juliacmd)
-export_markdown("benchmark.md", judge(MixedModels, config; verbose=true))
+# for many of the smaller models, we get a lot of noise at the default 5% tolerance
+# TODO: specify a tune.json with per model time tolerances
+export_markdown("benchmark.md", judge(MixedModels, config; verbose=true, retune=false, f=median, judgekwargs=(;time_tolerance=0.1, memory_tolerance=0.05)))
