bf0a6f1c7089394206b2cd0a637ed089368619c5
Phillip Alday <phillip.alday@beacon.bio>
2024-05-29 13:27:22 -0500

    just the fixed effects

--- ext/MixedModelsVcovExt.jl
+++ ext/MixedModelsVcovExt.jl
@@ -2,18 +2,21 @@ module MixedModelsVcovExt
 
 using MixedModels
 using MixedModels: StatsAPI
+using MixedModels.StatsModels: hasintercept
+
 using Vcov: Vcov, CovarianceEstimator, VcovData
 
-
-# in 0.8, Vcov uses invcrossmodelmatrix as an accessor function
-# for a precomputed inv(crossmodelmatrix(m))
-if pkgversion(Vcov) >= v"0.8"
-    Vcov.invcrossmodelmatrix(m::MixedModel) = inv(crossmodelmatrix(m))
-
-    function Vcov.VcovData(m::MixedModel)
-        cm = crossmodelmatrix(m)
-        ddf = nobs(m) - round(Int, sum(leverage(m)))
-        return VcovData(modelmatrix(m), cm, inv(cm), residuals(m), ddf)
+function Vcov.VcovData(m::MixedModel)
+    cm = crossmodelmatrix(m)
+    mm = modelmatrix(m)
+    # from just the fixed effects
+    ddf = nobs(m) - rank(m) - hasintercept(formula(m)) # dof_residual(m) # nobs(m) - round(Int, sum(leverage(m)))
+    resid = response(m) - mm * coef(m) # residuals(m)
+    @static if pkgversion(Vcov) >= v"0.8"
+        # Vcov 0.8 stores the inverse of crossmodelmatrix
+        return VcovData(mm, cm, inv(cm), resid, ddf)
+    else
+        return VcovData(mm, cm, resid, ddf)
     end
 
 end
@@ -22,7 +25,6 @@ end
 # relevant CovarianceEstimator
 StatsAPI.vcov(m::MixedModel, c::Vcov.SimpleCovariance) = vcov(VcovData(m), c)
 StatsAPI.vcov(m::MixedModel, c::Vcov.RobustCovariance) = vcov(VcovData(m), c)
-
-# Vcov.vcov(m::MixedModel, ::Vcov.SimpleCovariance) = vcov(m)
+# StatsAPI.vcov(m::MixedModel, c::Vcov.ClusterCovariance) = vcov(VcovData(m), c)
 
 end # module
