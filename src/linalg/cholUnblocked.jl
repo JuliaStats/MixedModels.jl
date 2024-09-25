4318cb3c058171c7f7cf87da659f158b01c30978
Phillip Alday <me@phillipalday.com>
2024-09-24 21:00:27 -0500

    cholUnBlocked performance tweaks

--- src/linalg/cholUnblocked.jl
+++ src/linalg/cholUnblocked.jl
@@ -10,7 +10,7 @@ function cholUnblocked! end
 
 function cholUnblocked!(D::Diagonal{T}, ::Type{Val{:L}}) where {T<:AbstractFloat}
     Ddiag = D.diag
-    @inbounds for i in eachindex(Ddiag)
+    @simd for i in eachindex(Ddiag)
         (ddi = Ddiag[i]) ≤ zero(T) && throw(PosDefException(i))
         Ddiag[i] = sqrt(ddi)
     end
@@ -38,7 +38,7 @@ end
 
 function cholUnblocked!(D::UniformBlockDiagonal, ::Type{Val{:L}})
     Ddat = D.data
-    for k in axes(Ddat, 3)
+    @inbounds for k in axes(Ddat, 3)
         cholUnblocked!(view(Ddat, :, :, k), Val{:L})
     end
     return D
