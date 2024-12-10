b771dbe16b1b0a46ecb0c37fae6423465b3d43e5
Phillip Alday <me@phillipalday.com>
2024-12-10 09:52:37 -0600

    use StableRNG

--- issues/792/main.jl
+++ issues/792/main.jl
@@ -1,15 +1,13 @@
-using MixedModels, DataFrames, Random
-Random.seed!(0)
+using MixedModels, DataFrames, StableRNGs
+rng = StableRNG(0)
 df = allcombinations(DataFrame,
                      "subject" => 1:10,
                      "session" => 1:6,
                      "serialpos" => 1:12)
-df[!, :recalled] = rand(nrow(df)) .> .5
+df[!, :recalled] = rand(rng, [0, 1], nrow(df))
 serialpos_contrasts = Dict(:serialpos => DummyCoding())
 
 form = @formula(recalled ~ serialpos + zerocorr(serialpos | subject) + (1 | subject & session))
-m = GeneralizedLinearMixedModel(form, df, Bernoulli(); contrasts=serialpos_contrasts)
-# m = GeneralizedLinearMixedModel(form, df, Bernoulli();
-#                                 contrasts=serialpos_contrasts)
-
-fit!(m; init_from_lmm=[:β, :θ])
+m = GeneralizedLinearMixedModel(form, df, Bernoulli(); contrasts=serialpos_contrasts);
+m.optsum.ftol_rel = 1e-7
+fit!(m; init_from_lmm=[:β, :θ], fast=true)
