9c50828b032e9b357a54a3e0f993db127aaa2226
Phillip Alday <me@phillipalday.com>
2024-12-10 09:43:41 -0600

    script tweajs

--- issues/792/main.jl
+++ issues/792/main.jl
@@ -1,17 +1,16 @@
 using MixedModels, DataFrames, Random
 Random.seed!(0)
 df = allcombinations(DataFrame,
-                     "subject" => 1:100,
+                     "subject" => 1:10,
                      "session" => 1:6,
                      "list" => 1:25,
                      "serialpos" => 1:12)
-df[!, :recalled] = rand(nrow(df))
-df[!, :recalled] = df[!, :recalled] .> .5
-
+df[!, :recalled] = rand(nrow(df)) .> .5
 serialpos_contrasts = Dict(:serialpos => DummyCoding())
 
-form = @formula(recalled ~ serialpos + zerocorr(fulldummy(serialpos) | subject) + (1 | subject & session))
-m = MixedModel(
-        form,
-        df, Bernoulli();
-        contrasts=serialpos_contrasts)
+form = @formula(recalled ~ serialpos + zerocorr(serialpos | subject) + (1 | subject & session))
+m = GeneralizedLinearMixedModel(form, df, Bernoulli(); contrasts=serialpos_contrasts)
+# m = GeneralizedLinearMixedModel(form, df, Bernoulli();
+#                                 contrasts=serialpos_contrasts)
+
+fit!(m; init_from_lmm=[:β, :θ])
