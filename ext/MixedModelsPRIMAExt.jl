465392efee68002f28e71d3398a3c398677d1062
Douglas Bates <dmbates@gmail.com>
2025-01-22 11:36:11 -0600

    Set optsum.feval in prfit!

--- ext/MixedModelsPRIMAExt.jl
+++ ext/MixedModelsPRIMAExt.jl
@@ -4,7 +4,11 @@ using MixedModels: MixedModels, LinearMixedModel, objective!
 using PRIMA: PRIMA
 
 function MixedModels.prfit!(m::LinearMixedModel)
-    PRIMA.bobyqa!(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
+    θ, info = PRIMA.bobyqa(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
+    if θ ≠ m.θ   # force an evaluation of the objective at the optimum
+        objective!(m, θ)
+    end
+    m.optsum.feval = info.nf
     return m
 end
 
