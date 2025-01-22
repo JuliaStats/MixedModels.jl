64cd86c43a29069a6b78cbf6bcbf9c7bcd171b79
Phillip Alday <me@phillipalday.com>
2025-01-21 23:46:30 -0600

    oops

--- ext/MixedModelsPRIMAExt.jl
+++ ext/MixedModelsPRIMAExt.jl
@@ -4,7 +4,8 @@ using MixedModels: MixedModels, LinearMixedModel, objective!
 using PRIMA: PRIMA
 
 function MixedModels.prfit!(m::LinearMixedModel)
-    return PRIMA.bobyqa!(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
+    PRIMA.bobyqa!(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
+    return m
 end
 
 end # module
