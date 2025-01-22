ba8928a3d54c75bbdca082f5b5fd8868fdf056e1
Douglas Bates <dmbates@gmail.com>
2025-01-22 11:48:34 -0600

    Revert to using bobyqa!

--- ext/MixedModelsPRIMAExt.jl
+++ ext/MixedModelsPRIMAExt.jl
@@ -4,10 +4,7 @@ using MixedModels: MixedModels, LinearMixedModel, objective!
 using PRIMA: PRIMA
 
 function MixedModels.prfit!(m::LinearMixedModel)
-    θ, info = PRIMA.bobyqa(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
-    if θ ≠ m.θ   # force an evaluation of the objective at the optimum
-        objective!(m, θ)
-    end
+    info = PRIMA.bobyqa!(objective!(m), copy(m.optsum.initial); xl=m.optsum.lowerbd)
     m.optsum.feval = info.nf
     return m
 end
