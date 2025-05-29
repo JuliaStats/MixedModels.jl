054861af3569333f8ae71f2725c61211243e37d9
Phillip Alday <palday@users.noreply.github.com>
2025-05-29 14:04:45 -0500

    Update src/mimeshow.jl
    Co-authored-by: Alex Arslan <ararslan@comcast.net>

--- src/mimeshow.jl
+++ src/mimeshow.jl
@@ -151,7 +151,7 @@ function _markdown(m::MixedModel)
     re_without_fe = setdiff!(
         # why not use `Set`? because ordering can change between releases, while
         # unique preserves the original order
-        unique(mapfoldl(x -> getproperty(x, :cnames), vcat, m.reterms)), coefnames(m)
+        unique!(mapfoldl(x -> getproperty(x, :cnames), append!, m.reterms; init=String[])), coefnames(m)
     )
 
     for bname in re_without_fe
